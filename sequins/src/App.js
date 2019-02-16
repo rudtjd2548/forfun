3c62dfea4051e60abc4eca3d503edbc09eeb6122
rudtjd2548 <a92667441@gmail.com>
2019-02-16 18:12:04 +1300

    screen resize optimized

--- sequins/src/App.js
+++ sequins/src/App.js
@@ -1,27 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useLayoutEffect, useEffect } from 'react';
 import './App.css';
 
-const DragContainer = () => {
+const DragContainer = ({ dimension, cellSize }) => {
   const [Isclicking, setIsClicking] = useState(false);
   const [clickedCells, setClickedCells] = useState([]);
+  const [totalBoxs, setTotalBoxs] = useState(0);
 
-  function mouseDownHandler(e) {
+  useEffect(() => {
+    const floorWidth = Math.floor(dimension.width / cellSize);
+    const floorHeight = Math.floor(dimension.height / cellSize);
+    setTotalBoxs(floorWidth * floorHeight);
+  }, [dimension]);
+
+  const mouseDownHandler = e => {
     e.preventDefault();
     setIsClicking(true);
 
     const cell = Number(e.target.getAttribute('name'));
+    const floorWidth = Math.floor(dimension.width / cellSize);
+    const cellRange = [
+      cell - 1,
+      cell,
+      cell + 1,
+      cell + floorWidth,
+      cell - floorWidth
+    ];
+    console.log([...cellRange]);
     clickedCells.find(el => el === cell)
       ? setClickedCells(clickedCells.filter(el => el !== cell))
-      : setClickedCells(prevState => [...prevState, cell]);
-  }
+      : setClickedCells(prevState => [...prevState, ...[...cellRange]]);
+  };
 
-  function mouseUpHandler(e) {
+  const mouseUpHandler = e => {
     e.preventDefault();
     setIsClicking(false);
-    console.log(e.target.getAttribute('name'));
-  }
+  };
 
-  function mouseOverHandler(e) {
+  const mouseOverHandler = e => {
     e.preventDefault();
     const cell = Number(e.target.getAttribute('name'));
     if (Isclicking) {
@@ -29,16 +44,15 @@ const DragContainer = () => {
         ? setClickedCells(clickedCells.filter(el => el !== cell))
         : setClickedCells(prevState => [...prevState, cell]);
     }
-  }
+  };
 
-  function mouseLeaveHandler(e) {
+  const mouseLeaveHandler = e => {
     e.preventDefault();
     setIsClicking(false);
-    console.log(e.target.getAttribute('name'));
-  }
+  };
 
   let cells = [];
-  for (let i = 1; i < 2000; i++) {
+  for (let i = 1; i < totalBoxs + 1; i++) {
     cells.push(
       <div
         key={i}
@@ -46,10 +60,11 @@ const DragContainer = () => {
         className={
           clickedCells.find(el => el === i) ? `Cell Cell-clicked` : 'Cell'
         }
+        style={{ width: `${cellSize}px`, height: `${cellSize}px` }}
       />
     );
   }
-
+  console.log('[DragContainer] is rendered');
   return (
     <div
       className='DragContainer'
@@ -64,9 +79,28 @@ const DragContainer = () => {
 };
 
 const App = () => {
+  const [dimension, setDimension] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
+  const [isResizing, SetIsResizing] = useState(false);
+  useLayoutEffect(() => {
+    if (!isResizing) {
+      window.addEventListener('resize', () => {
+        SetIsResizing(true);
+        setTimeout(() => {
+          SetIsResizing(false);
+        }, 2500);
+      });
+    }
+    setDimension({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+  }, [isResizing]);
   return (
     <div className='App'>
-      <DragContainer />
+      <DragContainer dimension={dimension} cellSize='15' />
     </div>
   );
 };
